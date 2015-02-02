2ae6710825e986675ea968ccb26bf34217337747
Prathamesh Shetye <p.shetye@sta.samsung.com>
2015-02-02 17:47:44 -0600

    Added the AsyncTask

--- app/src/main/java/app/supergallery/techeretic/com/filereader/ImageAdapter.java
+++ app/src/main/java/app/supergallery/techeretic/com/filereader/ImageAdapter.java
@@ -1,11 +1,15 @@
 package app.supergallery.techeretic.com.filereader;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +17,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +35,13 @@ public class ImageAdapter extends
 
     private Context mContext;
 
+    private int mHeight;
 
+    private int mWidth;
+
+    private int mSize;
+
+    private LruCache<String, Bitmap> mMemoryCache;
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
@@ -44,10 +55,28 @@ public class ImageAdapter extends
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ImageAdapter(List<String> objects, Context context) {
+    public ImageAdapter(List<String> objects, Context context, int height, int width) {
         Log.d(LOG_TAG, "Inside Constructor");
         mContext = context;
         mImagePaths = objects;
+        mHeight = height;
+        mWidth = width;
+        mSize = width / 3;
+        // Get memory class of this device, exceeding this amount will throw an
+        // OutOfMemory exception.
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in bytes rather than number
+                // of items.
+                return bitmap.getByteCount();
+            }
+
+        };
     }
 
     // Create new views (invoked by the layout manager)
@@ -66,17 +95,150 @@ public class ImageAdapter extends
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        File imgFile = new  File(mImagePaths.get(position));
+        /*File imgFile = new  File(mImagePaths.get(position));
         Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-        int nh = (int) ( myBitmap.getHeight() * (128.0 / myBitmap.getWidth()) );
-        Bitmap scaled = Bitmap.createScaledBitmap(myBitmap, 128, nh, true);
-        holder.mIView.setImageBitmap(scaled);
+        int nh = (int) ( myBitmap.getHeight() * (mSize / myBitmap.getWidth()) );
+        Bitmap scaled = Bitmap.createScaledBitmap(myBitmap, mSize, nh, true);
+        holder.mIView.setImageBitmap(scaled);*/
+        loadBitmap(position, holder.mIView);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mImagePaths.size();
+    }
+
+    public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+        public int data = 0;
+        private final WeakReference<ImageView> imageViewReference;
+
+        public BitmapWorkerTask(ImageView imageView) {
+            // Use a WeakReference to ensure the ImageView can be garbage
+            // collected
+            imageViewReference = new WeakReference<ImageView>(imageView);
+        }
+
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(Integer... params) {
+            data = params[0];
+            final Bitmap bitmap = decodeSampledBitmapFromResource(
+                    mImagePaths.get(data), mSize, mSize);
+            addBitmapToMemoryCache(mImagePaths.get(params[0]), bitmap);
+            return bitmap;
+        }
+
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (imageViewReference != null && bitmap != null) {
+                final ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(String filePath, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth,
+                reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and
+            // width
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will
+            // guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
+    }
+
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return (Bitmap) mMemoryCache.get(key);
+    }
+
+    public void loadBitmap(int position, ImageView imageView) {
+        if (cancelPotentialWork(position, imageView)) {
+            final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+            imageView.setBackgroundResource(R.drawable.empty_photo);
+            task.execute(position);
+        }
+    }
+
+    static class AsyncDrawable extends BitmapDrawable {
+        private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+
+        public AsyncDrawable(Resources res, Bitmap bitmap,
+                             BitmapWorkerTask bitmapWorkerTask) {
+            super(res, bitmap);
+            bitmapWorkerTaskReference = new WeakReference<BitmapWorkerTask>(
+                    bitmapWorkerTask);
+        }
+
+        public BitmapWorkerTask getBitmapWorkerTask() {
+            return bitmapWorkerTaskReference.get();
+        }
+    }
+
+    public static boolean cancelPotentialWork(int data, ImageView imageView) {
+        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+
+        if (bitmapWorkerTask != null) {
+            final int bitmapData = bitmapWorkerTask.data;
+            if (bitmapData != data) {
+                bitmapWorkerTask.cancel(true);
+            } else {
+                return false;
+            }
+        }
+        // No task associated with the ImageView, or an existing task was
+        // cancelled
+        return true;
+    }
+
+    private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
+        if (imageView != null) {
+            final Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof AsyncDrawable) {
+                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+                return asyncDrawable.getBitmapWorkerTask();
+            }
+        }
+        return null;
     }
 
 }
--- app/src/main/java/app/supergallery/techeretic/com/supergallery/RootActivity.java
+++ app/src/main/java/app/supergallery/techeretic/com/supergallery/RootActivity.java
@@ -1,15 +1,21 @@
 package app.supergallery.techeretic.com.supergallery;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 import app.supergallery.techeretic.com.filereader.FileHelper;
@@ -30,10 +36,17 @@ public class RootActivity extends ActionBarActivity {
         for (String path : mFilePaths) {
             Log.d("SuperGallery","path = " + path);
         }
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.ListView);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,
                 StaggeredGridLayoutManager.VERTICAL));
-        ImageAdapter imageAdapter = new ImageAdapter(mFilePaths, getApplicationContext());
+        ImageAdapter imageAdapter = new ImageAdapter(mFilePaths, getApplicationContext(), height, width);
         recyclerView.setAdapter(imageAdapter);
     }
 }
